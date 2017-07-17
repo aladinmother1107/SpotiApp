29613617431686998f72fb7940d4a58122ec0a16
aladinmother1107 <aladinmother1107@hotmail.com>
2017-07-17 14:02:04 +0200

    Version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule} from '@angular/http';
+import { FormsModule} from '@angular/forms';
 
 import { APP_ROUTING } from './app.routes';
 
@@ -19,6 +21,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     APP_ROUTING
   ],
   providers: [
